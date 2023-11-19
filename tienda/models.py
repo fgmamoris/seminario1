a58e0233ca7f56b65087a67270da673de514a8b3
Ayrton <asegura@fiscalias.gob.ar>
2023-11-18 21:07:35 -0300

    avances sobre el modificar

--- tienda/models.py
+++ tienda/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Producto(models.Model):
+    id = models.IntegerField(primary_key = True)
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     cantidad = models.IntegerField(default=0)
